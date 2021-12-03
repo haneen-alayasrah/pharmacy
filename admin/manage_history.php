52cac21152740c1974fe7e738cab44d27f908154
Hassanwael <hassanwael1999@gmail.com>
2021-12-03 21:00:30 +0200

    Manage User and Manage history

--- admin/manage_history.php
+++ admin/manage_history.php
@@ -1,6 +1,5 @@
+<?php include("includes/config.php  ");?>
 <?php include("includes/header.php"); ?>
-
-
 
 <div class="app-main">
 <?php include("includes/sidebar.php"); ?>
@@ -9,135 +8,44 @@
 
 
         <div class="app-main__inner">
-            <div class="col">
-                <div class="main-card mb-3 card">
-                    <div class="card-body">
-                        <h5 class="card-title">Admin</h5>
-                        <form class="needs-validation" novalidate>
-                            <div class="form-row">
-                                <div class="col-md-4 mb-3">
-                                    <label for="validationCustom01">First name</label>
-                                    <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="Mark" required>
-                                    <div class="valid-feedback">
-                                        Looks good!
-                                    </div>
-                                </div>
-                                <div class="col-md-4 mb-3">
-                                    <label for="validationCustom02">Last name</label>
-                                    <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Otto" required>
-                                    <div class="valid-feedback">
-                                        Looks good!
-                                    </div>
-                                </div>
-                                <div class="col-md-4 mb-3">
-                                    <label for="validationCustomUsername">Username</label>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="inputGroupPrepend">@</span>
-                                        </div>
-                                        <input type="text" class="form-control" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend" required>
-                                        <div class="invalid-feedback">
-                                            Please choose a username.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-row">
-                                <div class="col-md-6 mb-3">
-                                    <label for="validationCustom03">City</label>
-                                    <input type="text" class="form-control" id="validationCustom03" placeholder="City" required>
-                                    <div class="invalid-feedback">
-                                        Please provide a valid city.
-                                    </div>
-                                </div>
-                                <div class="col-md-3 mb-3">
-                                    <label for="validationCustom04">State</label>
-                                    <input type="text" class="form-control" id="validationCustom04" placeholder="State" required>
-                                    <div class="invalid-feedback">
-                                        Please provide a valid state.
-                                    </div>
-                                </div>
-                                <div class="col-md-3 mb-3">
-                                    <label for="validationCustom05">Zip</label>
-                                    <input type="text" class="form-control" id="validationCustom05" placeholder="Zip" required>
-                                    <div class="invalid-feedback">
-                                        Please provide a valid zip.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-                                    <label class="form-check-label" for="invalidCheck">
-                                        Agree to terms and conditions
-                                    </label>
-                                    <div class="invalid-feedback">
-                                        You must agree before submitting.
-                                    </div>
-                                </div>
-                            </div>
-                            <button class="btn btn-primary" type="submit">Submit form</button>
-                        </form>
-
-                        <script>
-                            // Example starter JavaScript for disabling form submissions if there are invalid fields
-                            (function() {
-                                'use strict';
-                                window.addEventListener('load', function() {
-                                    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-                                    var forms = document.getElementsByClassName('needs-validation');
-                                    // Loop over them and prevent submission
-                                    var validation = Array.prototype.filter.call(forms, function(form) {
-                                        form.addEventListener('submit', function(event) {
-                                            if (form.checkValidity() === false) {
-                                                event.preventDefault();
-                                                event.stopPropagation();
-                                            }
-                                            form.classList.add('was-validated');
-                                        }, false);
-                                    });
-                                }, false);
-                            })();
-                        </script>
-                    </div>
-
-                </div>
-            </div>
+            
 
 
             <div class="col-lg-12">
 
                 <div class="main-card mb-3 card">
                     <div class="card-body">
-                        <h5 class="card-title">Table striped</h5>
+                        <h5 class="card-title">History Table</h5>
                         <table class="mb-0 table table-striped">
                             <thead>
                                 <tr>
                                     <th>#</th>
-                                    <th>First Name</th>
-                                    <th>Last Name</th>
-                                    <th>Username</th>
+                                    <th>User_ID</th>
+                                    <th>Item Id</th>
+                                    <th>Order Date</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
-                                </tr>
+                                <?php
+                                $select = "SELECT * FROM history";
+                                $res = $conn->query($select);
+                                if($res){
+                                    while($row = $res->fetch_assoc()):
+                                        ?>
+                                        <tr>
+                                            <th scope="row"><?php echo $row['history_id'];?></th>
+                                            <td><?php echo $row['user_id'];?></td>
+                                            <td><?php echo $row['item_id'];?></td>
+                                            <td><?php echo $row['history_date'];?></td>
+                                        </tr>
+                                        <?php endwhile;?>
+                                        <?php
+                                }else{
+                                    echo "No data was found";
+                                    die();  
+                                }
+                                        ?>
+                               
                             </tbody>
                         </table>
                     </div>
--- admin/manage_users.php
+++ admin/manage_users.php
@@ -1,69 +1,95 @@
+<?php include("includes/config.php"); ?>
 <?php include("includes/header.php"); ?>
 
 
 
 <div class="app-main">
-<?php include("includes/sidebar.php"); ?>
+    <?php include("includes/sidebar.php"); ?>
 
     <div class="app-main__outer">
+        <?php
+        $user = array();
+        
+        if (isset($_POST['submit'])) {
+            $item_date= date('Y-m-d');
+            $user = [
+                'name' => $_POST['name'],
+                'email' => $_POST['email'],
+                'password' => $_POST['password']
+            ];
+            if (isset($_GET['method'])) {
+               
+            if($_GET['method']=='edit'){
+               
+            $insert = "UPDATE user SET user_fullname = '{$user['name']}',
+                                        user_email  = '{$user['email']}',
+                                        user_password = '{$user['password']}' where user_id = {$_GET['id']}";
+            if($conn->query($insert)){
+                echo "";  
+            } else echo $insert." -- ".$conn->error;  
+        }
+            }else{
+           
+            $insert = "INSERT INTO user(user_fullname,user_email,user_password,user_date_create)
+                              VALUES ('{$user['name']}','{$user['email']}','{$user['password']}','$item_date')";
+            if($conn->query($insert)){
+                echo "";  
+            } else echo $insert." -- ".$conn->error;  
+        }
 
+           
+        }
+        if(isset($_GET['method'])){
+            if($_GET['method']=='edit'){
+               
+               $select = "SELECT * FROM user where user_id = {$_GET['id']}";
+               
+                $res=$conn->query($select);
+                $result=$res->fetch_assoc();
+                
+             
+            }else{
+                $del = "Delete from user where user_id ={$_GET['id']} ";
+                $conn->query($del);
+            }
+        }
+
+        ?>
 
         <div class="app-main__inner">
             <div class="col">
                 <div class="main-card mb-3 card">
                     <div class="card-body">
                         <h5 class="card-title">Admin</h5>
-                        <form class="needs-validation" novalidate>
-                            <div class="form-row">
-                                <div class="col-md-4 mb-3">
-                                    <label for="validationCustom01">First name</label>
-                                    <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="Mark" required>
-                                    <div class="valid-feedback">
-                                        Looks good!
-                                    </div>
-                                </div>
-                                <div class="col-md-4 mb-3">
-                                    <label for="validationCustom02">Last name</label>
-                                    <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Otto" required>
-                                    <div class="valid-feedback">
-                                        Looks good!
-                                    </div>
-                                </div>
-                                <div class="col-md-4 mb-3">
-                                    <label for="validationCustomUsername">Username</label>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="inputGroupPrepend">@</span>
-                                        </div>
-                                        <input type="text" class="form-control" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend" required>
-                                        <div class="invalid-feedback">
-                                            Please choose a username.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                        <form method="POST" class="needs-validation" novalidate>
                             <div class="form-row">
                                 <div class="col-md-6 mb-3">
-                                    <label for="validationCustom03">City</label>
-                                    <input type="text" class="form-control" id="validationCustom03" placeholder="City" required>
+                                    <label for="validationCustom03">Email</label>
+                                    <input type="email" name="email" value="<?php echo @$result['user_email'];?>" class="form-control" id="validationCustom03" placeholder="E-mail" required>
                                     <div class="invalid-feedback">
-                                        Please provide a valid city.
+                                        Please provide a valid Email
                                     </div>
                                 </div>
+                                <div class="col-md-4 mb-3">
+                                    <label for="validationCustom01">Name</label>
+                                    <input type="text" name="name"  value="<?php echo @$result['user_fullname'];?>" class="form-control" id="validationCustom01" placeholder="First name" value="Mark" required>
+                                    <div class="valid-feedback">
+                                        Looks good!
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            <div class="form-row">
+
                                 <div class="col-md-3 mb-3">
-                                    <label for="validationCustom04">State</label>
-                                    <input type="text" class="form-control" id="validationCustom04" placeholder="State" required>
+                                    <label for="validationCustom04">Password</label>
+                                    <input type="password" name="password"  value="<?php echo @$result['user_password'];?>" class="form-control" id="validationCustom04" placeholder="Password" required>
                                     <div class="invalid-feedback">
-                                        Please provide a valid state.
+                                        Please provide a valid password.
                                     </div>
                                 </div>
-                                <div class="col-md-3 mb-3">
-                                    <label for="validationCustom05">Zip</label>
-                                    <input type="text" class="form-control" id="validationCustom05" placeholder="Zip" required>
-                                    <div class="invalid-feedback">
-                                        Please provide a valid zip.
-                                    </div>
-                                </div>
+
                             </div>
                             <div class="form-group">
                                 <div class="form-check">
@@ -76,7 +102,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <button class="btn btn-primary" type="submit">Submit form</button>
+                            <button class="btn btn-primary" type="submit" name="submit">Submit form</button>
                         </form>
 
                         <script>
@@ -106,7 +132,12 @@
 
 
             <div class="col-lg-12">
-
+                <?php
+                
+                $select = "SELECT * FROM user";
+                $res=$conn->query($select);
+              
+                ?>
                 <div class="main-card mb-3 card">
                     <div class="card-body">
                         <h5 class="card-title">Table striped</h5>
@@ -114,30 +145,39 @@
                             <thead>
                                 <tr>
                                     <th>#</th>
-                                    <th>First Name</th>
-                                    <th>Last Name</th>
-                                    <th>Username</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Last-Login</th>
+                                    <th>Date-Created</th>
+                                    <th> Edit  </th>
+                                    <th> Del</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                <?php while($row =$res->fetch_assoc()):?>
                                 <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
+                                    <th scope="row"><?php echo $row['user_id']?></th>
+                                    <td><?php echo $row['user_fullname']?></td>
+                                    <td><?php echo $row['user_email']?></td>
+                                    <td><?php echo $row['user_last_login']?></td>
+                                    <td><?php echo $row['user_date_create']?></td>
+                                    <td>
+                                        <a href="manage_users.php?method=edit&&id=<?php echo $row['user_id'];?>">
+                                        <button class="btn btn-success btn-sm">
+                                            Edit
+                                        </button>
+                                        </a>
+                                        
+                                    </td>
+                                    <td>
+                                    <a href="manage_users.php?method=del&&id=<?php echo $row['user_id'];?>">
+                                        <button class="btn btn-danger btn-sm">
+                                            Delete
+                                        </button>
+                                        </a>
+                                    </td>
                                 </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
-                                </tr>
+                                <?php endwhile;?>
                             </tbody>
                         </table>
                     </div>

8626e6e2955b13c431a20142d56c5c597a7b1054
Omar-alrantisi <omaratef.alrantisi@gmail.com>
2021-12-04 20:22:26 +0200

    landing page

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
+
+                                
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -52,34 +55,30 @@ https://templatemo.com/tm-571-hexashop
                     <nav class="main-nav">
                         <!-- ***** Logo Start ***** -->
                         <a href="index.html" class="logo">
-                            <img src="assets/images/logo.png">
+                            <img src="assets/images/logoo.png" width="100px" height="80px">
                         </a>
                         <!-- ***** Logo End ***** -->
                         <!-- ***** Menu Start ***** -->
                         <ul class="nav">
-                            <li class="scroll-to-section"><a href="index.html" class="active">Home</a></li>
-                            <li class="scroll-to-section"><a href="index.html">Men's</a></li>
-                            <li class="scroll-to-section"><a href="index.html">Women's</a></li>
-                            <li class="scroll-to-section"><a href="index.html">Kid's</a></li>
+                            <li class="scroll-to-section"><a href="index.php" class="active">Home</a></li>
+                           
                             <li class="submenu">
-                                <a href="javascript:;">Pages</a>
+                                <a href="javascript:;">Products</a>
                                 <ul>
-                                    <li><a href="about.html">About Us</a></li>
-                                    <li><a href="products.html">Products</a></li>
-                                    <li><a href="single-product.html">Single Product</a></li>
-                                    <li><a href="contact.html">Contact Us</a></li>
+                                <li class="scroll-to-section"><a href="products.php?id=1">COVID Testing Kits</a></li>
+                            <li class="scroll-to-section"><a href="products.php?id=2">Quit Smoking</a></li>
+                            <li class="scroll-to-section"><a href="products.php?id=3">Face Coverings & Masks</a></li>
+                            <li class="scroll-to-section"><a href="products.php?id=4">children Health</a></li>
                                 </ul>
                             </li>
-                            <li class="submenu">
-                                <a href="javascript:;">Features</a>
-                                <ul>
-                                    <li><a href="#">Features Page 1</a></li>
-                                    <li><a href="#">Features Page 2</a></li>
-                                    <li><a href="#">Features Page 3</a></li>
-                                    <li><a rel="nofollow" href="https://templatemo.com/page/4" target="_blank">Template Page 4</a></li>
-                                </ul>
-                            </li>
-                            <li class="scroll-to-section"><a href="index.html">Explore</a></li>
+                            <li><a href="about.php">About Us</a></li>
+                                   
+                                   
+                                   <li><a href="contact.php">Contact Us</a></li>
+                                   <li><a href="account/signup.php">Sign Up</a></li>
+                                   <li><a href="account/login.php">Log In</a></li>
+                           
+                           
                         </ul>        
                         <a class='menu-trigger'>
                             <span>Menu</span>

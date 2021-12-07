40e0724e490c10288a7ca8e81beaf709fe85bf5f
Hassan Wael <hassanwael1999@gmail.com>
2021-12-07 19:00:27 +0200

    Update manage_comments.php

--- admin/manage_comments.php
+++ admin/manage_comments.php
@@ -43,12 +43,13 @@ session_start();
                   <th># Comment Id</th>
                   <th>User Id</th>
                   <th>Comment Statement</th>
+                  <th>Item Name</th>
                   <th>Delete</th>
                 </tr>
               </thead>
               <tbody>
                 <?php
-                  $fetch_query = "SELECT * FROM comments";
+                  $fetch_query = "SELECT comments.comment_id, comments.user_id,comments.comment_statment,comments.item_id,item.item_id,item_name FROM comments,item where comments.item_id = item.item_id";
                   $result = $conn->query($fetch_query);
                 ?>
                 <?php if ($result->num_rows > 0) : ?>
@@ -57,6 +58,7 @@ session_start();
                       <th scope="row"><?php echo $row["comment_id"]; ?></th>
                       <td><?php echo $row["user_id"]; ?></td>
                       <td><?php echo $row["comment_statment"]; ?></td>
+                      <td><?php echo $row["item_name"]; ?></td>
                       <td>
                         <a href="manage_comments.php?method=delete&&id=<?php echo $row["comment_id"] ?>">
                         <button type="submit"  class="btn btn-danger btn-sm">DELETE</button>
